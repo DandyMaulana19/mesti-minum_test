6eac18922c28a30b6b3fed3478cdde1617bfebd0
DandyMaulana19 <dandyainul19@gmail.com>
2025-04-22 16:40:13 +0700

    test: complete logout code

--- cypress/e2e/Logout/success-logout.cy.js
+++ cypress/e2e/Logout/success-logout.cy.js
@@ -1,11 +1,13 @@
-import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import { When, Then, Given } from "@badeball/cypress-cucumber-preprocessor";
 
 beforeEach(() => {
   cy.login("user1@gmail.com", "password");
 });
 
 When("user klik tombol logout", () => {
-  cy.get(".relative > button > .isax").click();
+  cy.visit("/");
+  cy.get(".relative > button > .isax");
+  cy.get(".py-1 > .block").click({ force: true });
 });
 
 Then("user diarahkan ke halaman utama setelah logout", () => {
